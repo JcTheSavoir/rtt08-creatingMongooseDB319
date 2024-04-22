877e2497ebd991023bfd05e3dde498728cb1f341
JcTheSavior <jacobconnell3@gmail.com>
2024-04-22 16:07:49 -0400

    Add Controller for the different routes

--- server.js
+++ server.js
@@ -5,62 +5,41 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const connectToDb = require("./config/connectToDb")
 // This pulls our Mongoose connection into application
+
+const Note = require("./models/note");
+const notesController = require('./controllers/notesController')
 const cors = require("cors"); 
 // ------------- Recieving reqs on cross-origins **
 app.use(express.json())
-// Express doesn't naturally convert our data to json
+// Express doesn"t naturally convert our data to json
 app.use(cors())
 connectToDb()
 // This initializes our connectToDb() function
-
 // ------------------------------------------------------reQs
 
-
-
 // -----------------------------------------------------ROUTING
-
-app.get('/', (req, res) => {
-    res.send("This is a Landing Page")
-})
-
-
 //We want to establish CRUD routes for our Notes Model-->
 //---{IMPORTANT}, every MODEL needs it's own Routes that include CREATE, Read, Update and Delete---{IMPORTANT}
-
-
-app.get('/notes,', async ()=>{  //READ
-    
-})
-//-------------------> GET all Notes
-
-app.get("/notes/:id", async ()=>{  //READ
-    
-})
-// ----------------------> GET a Specific Note by ID
-// ------------------------------------------------READ
-
-app.post('/notes', {   //CREATE
-
-})
-// ---------------------> Create a Note
 // ------------------------------------------------CREATE
-
-app.put('/notes/:id',{  //UPDATE
-
-})
+// ------------------------------------------------READ
 // ------------------------------------------------UPDATE
-
-app.delete('/notes/:id')  //DELETE
 // ------------------------------------------------DELETE
+
+app.get("/", (req, res) => {
+    res.send("This is a Landing Page")
+})
+//We want to establish CRUD routes for our Notes Model-->
+app.get("/notes", notesController.fetchAllNotes)
+//-----------------------------------------------------------------> GET all Notes -- [READ / GET]
+app.get("/notes/:id", notesController.fetchNote)
+// -------------------------------------------------------> GET a Specific Note by ID - [READ / GET]
+app.post("/notes", notesController.createNote);
+// ------------------------------------------------------------> Create a Note - [CREATE / POST] 
+app.put("/notes/:id", notesController.updateNote);
+//-----------------------------------------------------------> Update a Specific Note - [UPDATE]
+app.delete("/notes/:id", notesController.deleteNote);
+//-------------------------------------------------------------> Delete a specific Note - [DELETE]
 // - -  - - - - -  - - - - - - - ---  -- - - - - - -  - - - - - - - -
-
-
-
-
-
-
-
-
 
 
 app.listen(PORT, ()=>{
